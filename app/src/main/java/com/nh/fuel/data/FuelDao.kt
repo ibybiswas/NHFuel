3e17e86289a54905d7d3557c5ac393218b0abe16
ibybiswas <ibnebony100@gmail.com>
2026-07-27 21:44:03 +0530

    Remove FuelDatabase class and related imports

--- app/src/main/java/com/nh/fuel/data/FuelDao.kt
+++ app/src/main/java/com/nh/fuel/data/FuelDao.kt
@@ -12,14 +12,3 @@ interface FuelDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertOrUpdate(record: DailyFuelRecord)
 }
-
-// FuelDatabase.kt
-package com.nh.fuel.data
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [DailyFuelRecord::class], version = 1, exportSchema = false)
-abstract class FuelDatabase : RoomDatabase() {
-    abstract fun fuelDao(): FuelDao
-}
