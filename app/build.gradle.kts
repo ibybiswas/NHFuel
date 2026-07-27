5518b8385e293efd77100cad266f04a584bbc80d
ibybiswas <ibnebony100@gmail.com>
2026-07-27 22:40:06 +0530

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,5 +65,14 @@ dependencies {
 
     // Google Drive / Cloud Sync Client
     implementation("com.google.android.gms:play-services-auth:20.7.0")
-    implementation("com.google.apis:google-api-services-drive:v3-rev20230822-2.0.0")
+    implementation("com.google.apis:google-api-services-drive:v3-rev20230822-2.0.0") {
+        exclude(group = "org.apache.httpcomponents")
+        exclude(module = "guava-jdk5")
+    }
+    implementation("com.google.api-client:google-api-client-android:2.4.0") {
+        exclude(group = "org.apache.httpcomponents")
+        exclude(group = "com.google.android", module = "android")
+        exclude(module = "xpp3")
+    }
+    implementation("com.google.http-client:google-http-client-gson:1.44.1")
 }
