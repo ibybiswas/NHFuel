1b1859147d4fe84f4717779f59c122d3eb1d7bef
ibybiswas <ibnebony100@gmail.com>
2026-07-28 01:17:27 +0530

    Update FuelDatabase version and add TypeConverters
    Increment database version and add TypeConverters.

--- app/src/main/java/com/nh/fuel/data/FuelDatabase.kt
+++ app/src/main/java/com/nh/fuel/data/FuelDatabase.kt
@@ -1,10 +1,11 @@
-// FuelDatabase.kt
 package com.nh.fuel.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
-@Database(entities = [DailyFuelRecord::class], version = 1, exportSchema = false)
+@Database(entities = [DailyFuelRecord::class], version = 2, exportSchema = false)
+@TypeConverters(FuelConverters::class)
 abstract class FuelDatabase : RoomDatabase() {
     abstract fun fuelDao(): FuelDao
 }
