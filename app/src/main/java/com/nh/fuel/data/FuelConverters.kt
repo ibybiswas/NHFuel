75d174b83f63cc41252df0f4ba72c8f09d1b6597
ibybiswas <ibnebony100@gmail.com>
2026-07-28 12:15:30 +0530

    Add converters for RefillEvent in FuelConverters

--- app/src/main/java/com/nh/fuel/data/FuelConverters.kt
+++ app/src/main/java/com/nh/fuel/data/FuelConverters.kt
@@ -6,9 +6,17 @@ import com.google.gson.Gson
 class FuelConverters {
     private val gson = Gson()
 
+    // DayShift Converters
     @TypeConverter
     fun fromDayShift(shift: DayShift): String = gson.toJson(shift)
 
     @TypeConverter
     fun toDayShift(json: String): DayShift = gson.fromJson(json, DayShift::class.java)
+
+    // RefillEvent Converters
+    @TypeConverter
+    fun fromRefillEvent(event: RefillEvent): String = gson.toJson(event)
+
+    @TypeConverter
+    fun toRefillEvent(json: String): RefillEvent = gson.fromJson(json, RefillEvent::class.java)
 }
