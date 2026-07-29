498099b267c310e21e94fa8c2ccf35d2b04cab7b
ibybiswas <ibnebony100@gmail.com>
2026-07-29 13:27:19 +0530

    Increment database version from 3 to 4

--- app/src/main/java/com/nh/fuel/data/FuelDatabase.kt
+++ app/src/main/java/com/nh/fuel/data/FuelDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [DailyFuelRecord::class], version = 3, exportSchema = false)
+@Database(entities = [DailyFuelRecord::class], version = 4, exportSchema = false)
 @TypeConverters(FuelConverters::class)
 abstract class FuelDatabase : RoomDatabase() {
     abstract fun fuelDao(): FuelDao
