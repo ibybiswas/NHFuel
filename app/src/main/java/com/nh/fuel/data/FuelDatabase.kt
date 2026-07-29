8b0f593fec5dc249c4f3e8e8a2e2bc813224d242
ibybiswas <ibnebony100@gmail.com>
2026-07-30 01:15:42 +0530

    Add ExpenseItem entity to FuelDatabase

--- app/src/main/java/com/nh/fuel/data/FuelDatabase.kt
+++ app/src/main/java/com/nh/fuel/data/FuelDatabase.kt
@@ -4,8 +4,9 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [DailyFuelRecord::class], version = 4, exportSchema = false)
+@Database(entities = [DailyFuelRecord::class, ExpenseItem::class], version = 5, exportSchema = false)
 @TypeConverters(FuelConverters::class)
 abstract class FuelDatabase : RoomDatabase() {
     abstract fun fuelDao(): FuelDao
+    abstract fun expenseDao(): ExpenseDao
 }
